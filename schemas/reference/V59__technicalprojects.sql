c3402aa9e476696da2d2dbb992ed86b1704b7c56
Mark Olliver <mark@digitalpatterns.io>
2019-05-15 13:45:39 +0100

    updated technical projects

--- schemas/reference/V59__technicalprojects.sql
+++ schemas/reference/V59__technicalprojects.sql
@@ -1,6 +1,5 @@
 CREATE TABLE technicalprojects (
   id uuid NOT NULL PRIMARY KEY,
-  groupid uuid NOT NULL REFERENCES projectgroup(id),
   teamid uuid NOT NULL REFERENCES team(id),
   name varchar(100) NOT NULL,
   code varchar(10) NOT NULL,
@@ -13,7 +12,6 @@ GRANT SELECT ON technicalprojects TO ${readonlyuser};
 
 COMMENT ON TABLE technicalprojects IS '{"description": "Technical Projects", "schemalastupdated": "02/05/2019", "dataversion": 1}';
 COMMENT ON COLUMN technicalprojects.id IS '{"label": "Identifier", "description": "Unique identifying column", "summaryview": "false"}';
-COMMENT ON COLUMN technicalprojects.groupid IS '{"label": "Project Group ID", "description": "Project responsible group", "summaryview": "true"}';
 COMMENT ON COLUMN technicalprojects.name IS '{"label": "Name", "description": "Project name", "summaryview": "true"}';
 COMMENT ON COLUMN technicalprojects.teamid IS '{"label": "Team", "description": "Team the project belongs to", "summaryview": "true"}';
 COMMENT ON COLUMN technicalprojects.code IS '{"label": "Project code", "description": "Short code for the project", "summaryview": "true"}';
