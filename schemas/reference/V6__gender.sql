31b029f010136477d313059eda460ee596e9ca05
James Poulten <jpoulten@me.com>
2019-06-21 13:51:01 +0100

    Grammatical update - batch 2 a

--- schemas/reference/V6__gender.sql
+++ schemas/reference/V6__gender.sql
@@ -6,12 +6,12 @@ CREATE TABLE gender (
 );
 
 -- Table comment
-COMMENT ON TABLE gender IS '{"label": "Gender", "description": "A list of current groups on the gender spectrum", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE gender IS '{"label": "Gender", "description": "A list of current groups on the gender spectrum.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
-COMMENT ON COLUMN gender.id IS '{"label": "Identifier", "description": "Database unique identity record", "summaryview": "false"}';
-COMMENT ON COLUMN gender.gender IS '{"label": "Gender", "description": "Name of gender", "summaryview": "true"}';
-COMMENT ON COLUMN gender.validfrom IS '{"label": "Valid from date", "description": "Item valid from date", "summaryview" : "false"}';
-COMMENT ON COLUMN gender.validto IS '{"label": "Valid to date", "description": "Item valid to date", "summaryview" : "false"}';
+COMMENT ON COLUMN gender.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
+COMMENT ON COLUMN gender.gender IS '{"label": "Gender", "description": "Name of gender.", "summaryview": "true"}';
+COMMENT ON COLUMN gender.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN gender.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
 -- GRANTs
 GRANT SELECT ON gender TO ${anonuser};
--- schemas/reference/V8__title.sql
+++ schemas/reference/V8__title.sql
@@ -6,12 +6,12 @@ CREATE TABLE title (
 );
 
 -- Table comment
-COMMENT ON TABLE title IS '{"label": "Title", "description": ""A list of titles to be used when addressing a person", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE title IS '{"label": "Title", "description": ""A list of titles to be used when addressing a person.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
-COMMENT ON COLUMN title.id IS '{"label": "Identifier", "description": "Database unique identity record", "summaryview": "false"}';
-COMMENT ON COLUMN title.title IS '{"label": "Title", "description": "Name of title", "summaryview": "true"}';
-COMMENT ON COLUMN title.validfrom IS '{"label": "Valid from date", "description": "Item valid from date", "summaryview" : "false"}';
-COMMENT ON COLUMN title.validto IS '{"label": "Valid to date", "description": "Item valid to date", "summaryview" : "false"}';
+COMMENT ON COLUMN title.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
+COMMENT ON COLUMN title.title IS '{"label": "Title", "description": "Name of title.", "summaryview": "true"}';
+COMMENT ON COLUMN title.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN title.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
 -- GRANTs
 GRANT SELECT ON title TO ${anonuser};
