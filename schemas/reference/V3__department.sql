c78f7d7d8dff8e0b4573e60c76d4cd726cec5c3b
James Poulten <jpoulten@me.com>
2019-06-21 13:47:47 +0100

    Grammatical update - batch 1

--- schemas/reference/V3__department.sql
+++ schemas/reference/V3__department.sql
@@ -8,14 +8,14 @@ CREATE TABLE department (
 );
 
 -- Table comment
-COMMENT ON TABLE department IS '{"label": "Department", description": "A list of departments that make up the Home Office", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE department IS '{"label": "Department", description": "A list of departments that make up the Home Office.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
-COMMENT ON COLUMN department.id IS '{"description": "Database unique identity record", "summaryview": "false"}';
-COMMENT ON COLUMN department.name IS '{"description": "Name of department", "summaryview": "true"}';
-COMMENT ON COLUMN department.code IS '{"description": "Code of department", "summaryview": "true"}';
-COMMENT ON COLUMN department.validfrom IS '{"label": "Valid from date", "description": "Item valid from date", "summaryview" : "false"}';
-COMMENT ON COLUMN department.validto IS '{"label": "Valid to date", "description": "Item valid to date", "summaryview" : "false"}';
-COMMENT ON COLUMN department.ministryid IS '{"label": "Ministry ID", "description": "Link to ministry entity", "summaryview" : "false"}';
+COMMENT ON COLUMN department.id IS '{"description": "Database unique identity record.", "summaryview": "false"}';
+COMMENT ON COLUMN department.name IS '{"description": "Name of department.", "summaryview": "true"}';
+COMMENT ON COLUMN department.code IS '{"description": "Code of department.", "summaryview": "true"}';
+COMMENT ON COLUMN department.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN department.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
+COMMENT ON COLUMN department.ministryid IS '{"label": "Ministry ID", "description": "Link to ministry entity.", "summaryview" : "false"}';
 
 -- GRANTs
 GRANT SELECT ON department TO ${serviceuser};
--- schemas/reference/V4__directorate.sql
+++ schemas/reference/V4__directorate.sql
@@ -8,14 +8,14 @@ CREATE TABLE directorate (
 );
 
 -- Table comment
-COMMENT ON TABLE directorate IS '{"label": "Directorate names", "description": "Names of the departmental directorate", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE directorate IS '{"label": "Directorate names", "description": "Names of the departmental directorate.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
-COMMENT ON COLUMN directorate.id IS '{"label": "Identifier","description": "Database unique identity record", "summaryview": "false"}';
-COMMENT ON COLUMN directorate.name IS '{"label": "Name","description": "Name of directorate", "summaryview": "true"}';
-COMMENT ON COLUMN directorate.code IS '{"label": "Code","description": "Code of directorate", "summaryview": "true"}';
-COMMENT ON COLUMN directorate.validfrom IS '{"label": "Valid from date", "description": "Item valid from date", "summaryview" : "false"}';
-COMMENT ON COLUMN directorate.validto IS '{"label": "Valid to date", "description": "Item valid to date", "summaryview" : "false"}';
-COMMENT ON COLUMN directorate.departmentid IS '{"label": "Department ID", "description": "Link to department entity", "summaryview" : "false"}';
+COMMENT ON COLUMN directorate.id IS '{"label": "Identifier","description": "Database unique identity record.", "summaryview": "false"}';
+COMMENT ON COLUMN directorate.name IS '{"label": "Name","description": "Name of directorate.", "summaryview": "true"}';
+COMMENT ON COLUMN directorate.code IS '{"label": "Code","description": "Code of directorate.", "summaryview": "true"}';
+COMMENT ON COLUMN directorate.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN directorate.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
+COMMENT ON COLUMN directorate.departmentid IS '{"label": "Department ID", "description": "Link to department entity.", "summaryview" : "false"}';
 
 -- GRANTs
 GRANT SELECT ON directorate TO ${serviceuser};
