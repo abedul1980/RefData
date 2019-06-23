d10ec3eac36ccc159a6c71285e4d15d9ff2be2ae
James Poulten <jpoulten@me.com>
2019-06-23 14:47:51 +0100

    Gramma a

--- schemas/reference/V28__icao.sql
+++ schemas/reference/V28__icao.sql
@@ -18,10 +18,10 @@ COMMENT ON TABLE icao IS '{"label": "ICAO list", "description": "The ICAO worldw
 -- Column comments
 COMMENT ON COLUMN icao.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN icao.iso31661alpha2 IS '{"label": "2 digit alpha code", "description": "The countries 2 Character alpha code.", "summaryview": "true"}';
-COMMENT ON COLUMN icao.name IS '{"label": "Name", "description": "The ICAO site name.", "summaryview": "true"}';
-COMMENT ON COLUMN icao.iata IS '{"label": "IATA", "description": "The IATA code.", "summaryview": "true"}';
+COMMENT ON COLUMN icao.name IS '{"label": "Name", "description": "ICAO site name", "summaryview": "true"}';
+COMMENT ON COLUMN icao.iata IS '{"label": "IATA", "description": "IATA code", "summaryview": "true"}';
 COMMENT ON COLUMN icao.municipality IS '{"label": "City", "description": "Municipality/region", "summaryview": "true", "aliases": ["city", "region"]}';
-COMMENT ON COLUMN icao.icao IS '{"label": "ICAO code", "description": "The ICAO code.", "summaryview": "true"}';
+COMMENT ON COLUMN icao.icao IS '{"label": "ICAO code", "description": "ICAO code", "summaryview": "true"}';
 COMMENT ON COLUMN icao.geolat IS '{"label": "Latitude", "description": "Geographic latitude", "summaryview": "false"}';
 COMMENT ON COLUMN icao.geolong IS '{"label": "Longitude", "description": "Geographic longitude", "summaryview": "false"}';
 COMMENT ON COLUMN icao.type IS '{"label": "Port type", "description": "The type of port.", "summaryview": "true"}';
