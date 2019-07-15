d452fcd61c88d396598ef92ecfa15f725f79f56a
James Poulten <jpoulten@me.com>
2019-07-15 17:09:14 +0100

    New schema for selection indicator RefData

---
+++ schemas/reference/V60__selectionindicator.sql
@@ -0,0 +1,20 @@
+CREATE TABLE selectionindicator(
+  id INTEGER NOT NULL PRIMARY KEY, 
+  mode VARCHAR NOT NULL REFERENCES modeoftransport(id),
+  indicator VARCHAR NOT NULL,
+  validfrom TIMESTAMP WITH TIME ZONE,
+  validto TIMESTAMP WITH TIME ZONE
+);
+
+-- Table comment
+COMMENT ON TABLE selectionindicator IS '{"label": "Selection Indicator", "description": "A list of reasons for conducting a search on a particular vehical.", "schemalastupdated": "15/07/2019", "dataversion": 1}';
+-- Column comments
+COMMENT ON COLUMN selectionindicator.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
+COMMENT ON COLUMN selectionindicator.mode IS '{"label": "Mode of transport", "description": "The mode of transport used.", "summaryview": "true"}';
+COMMENT ON COLUMN selectionindicator.indicator IS '{"description": "Reason for selecting vehical for search.", "summaryview": "true"}';
+COMMENT ON COLUMN selectionindicator.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN selectionindicator.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
+
+-- GRANTs
+GRANT SELECT ON selectionindicator TO ${serviceuser};
+GRANT SELECT ON selectionindicator TO ${readonlyuser};
