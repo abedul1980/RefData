3ef0209f81eb39b69d8c92b42d622e5b327bdb2e
James Poulten <jpoulten@me.com>
2019-07-16 10:56:35 +0100

    Spelling and type change.

--- schemas/reference/V60__selectionindicator.sql
+++ schemas/reference/V60__selectionindicator.sql
@@ -1,17 +1,17 @@
 CREATE TABLE selectionindicator (
   id INTEGER NOT NULL PRIMARY KEY, 
-  mode VARCHAR NOT NULL REFERENCES targetmode(id),
+  mode INT4 NOT NULL REFERENCES targetmode(id),
   indicator VARCHAR NOT NULL,
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
 
 -- Table comment
-COMMENT ON TABLE selectionindicator IS '{"label": "Selection Indicator", "description": "A list of reasons for conducting a search on a particular vehical.", "schemalastupdated": "15/07/2019", "dataversion": 1}';
+COMMENT ON TABLE selectionindicator IS '{"label": "Selection Indicator", "description": "A list of reasons for conducting a search on a particular vehicle.", "schemalastupdated": "15/07/2019", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN selectionindicator.id IS '{"label": "Identifier", "description": "Database unique identity record.", "summaryview": "false"}';
 COMMENT ON COLUMN selectionindicator.mode IS '{"label": "Mode of transport", "description": "The mode of transport used.", "summaryview": "true"}';
-COMMENT ON COLUMN selectionindicator.indicator IS '{"description": "Reason for selecting vehical for search.", "summaryview": "true"}';
+COMMENT ON COLUMN selectionindicator.indicator IS '{"label": "Indicator", "description": "Reason for selecting vehicle for search.", "summaryview": "true"}';
 COMMENT ON COLUMN selectionindicator.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN selectionindicator.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
