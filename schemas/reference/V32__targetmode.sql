d5bb1ad82f183021127aab426e601366bb65328e
Mark Olliver <mark@digitalpatterns.io>
2019-07-16 15:56:23 +0100

    switch flyway base container and update target modes

--- schemas/reference/V32__targetmode.sql
+++ schemas/reference/V32__targetmode.sql
@@ -1,6 +1,7 @@
 CREATE TABLE targetmode (
   id INT4 NOT NULL PRIMARY KEY,
   mode VARCHAR(60) NOT NULL,
+  ien BOOL NOT NULL,
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
@@ -9,7 +10,8 @@ CREATE TABLE targetmode (
 COMMENT ON TABLE targetmode IS '{"label": "Target Modes", "description": "A list of target modes.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
 COMMENT ON COLUMN targetmode.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
-COMMENT ON COLUMN targetmode.type IS '{"label": "Target mode", "description": "The targeting mode type.", "summaryview": "true"}';
+COMMENT ON COLUMN targetmode.mode IS '{"label": "Target mode", "description": "The targeting mode type.", "summaryview": "true"}';
+COMMENT ON COLUMN targetmode.ien IS '{"label": "IEN enabled", "description": "Valid for IEN", "summaryview": "false"}';
 COMMENT ON COLUMN targetmode.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN targetmode.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
