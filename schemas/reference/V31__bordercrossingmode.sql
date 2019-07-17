9d8193b7bec0468938ba2e09494adf044ac359d4
Tom Fitzherbert <me@tomfitzherbert.com>
2019-07-17 11:16:14 +0100

    Fixes syntax error (and inconsistent punctuation)

--- schemas/reference/V31__bordercrossingmode.sql
+++ schemas/reference/V31__bordercrossingmode.sql
@@ -8,14 +8,14 @@ CREATE TABLE bordercrossingmode (
 );
 
 -- Table comment
-COMMENT ON TABLE bordercrossingmode IS '{"label": "Type of Border Crossing", "description": Methods of crossing the border, "schemalastupdated": "06/03/2019", "dataversion": 1}';
+COMMENT ON TABLE bordercrossingmode IS '{"label": "Type of Border Crossing", "description": "Methods of crossing the border", "schemalastupdated": "06/03/2019", "dataversion": 1}';
 -- Column comments
-COMMENT ON COLUMN bordercrossingmode.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
+COMMENT ON COLUMN bordercrossingmode.id IS '{"label": "Identifier", "description": "Unique identifying column", "summaryview": "false"}';
 COMMENT ON COLUMN bordercrossingmode.border IS '{"label": "Border", "description": "Type of border", "summaryview": "true"}';
 COMMENT ON COLUMN bordercrossingmode.type IS '{"label": "Service type", "description": "Is the service commercial or general", "summaryview": "true"}';
 COMMENT ON COLUMN bordercrossingmode.personorfreight IS '{"label": "Person or Freight", "description": "Is it a person or freight crossing the border", "summaryview": "true"}';
-COMMENT ON COLUMN bordercrossingmode.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
-COMMENT ON COLUMN bordercrossingmode.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
+COMMENT ON COLUMN bordercrossingmode.validfrom IS '{"label": "Valid from date", "description": "Item valid from date", "summaryview" : "false"}';
+COMMENT ON COLUMN bordercrossingmode.validto IS '{"label": "Valid to date", "description": "Item valid to date", "summaryview" : "false"}';
 
 -- GRANTs
 GRANT SELECT ON bordercrossingmode TO ${anonuser};
