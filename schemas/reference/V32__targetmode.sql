016768863a0043fa1d3e6d028403d6d0281e12a5
Mark Olliver <mark@digitalpatterns.io>
2019-07-17 15:58:47 +0100

    update schema for CA/CT

--- schemas/reference/V32__targetmode.sql
+++ schemas/reference/V32__targetmode.sql
@@ -2,6 +2,8 @@ CREATE TABLE targetmode (
   id INT4 NOT NULL PRIMARY KEY,
   mode VARCHAR(60) NOT NULL,
   ien BOOL NOT NULL,
+  ca BOOL NOT NULL,
+  ct BOOL NOT NULL,
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
@@ -11,7 +13,9 @@ COMMENT ON TABLE targetmode IS '{"label": "Target Modes", "description": "A list
 -- Column comments
 COMMENT ON COLUMN targetmode.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
 COMMENT ON COLUMN targetmode.mode IS '{"label": "Target mode", "description": "The targeting mode type.", "summaryview": "true"}';
-COMMENT ON COLUMN targetmode.ien IS '{"label": "IEN enabled", "description": "Valid for IEN", "summaryview": "false"}';
+COMMENT ON COLUMN targetmode.ien IS '{"label": "IEN enabled", "description": "Valid for IEN", "summaryview": "true"}';
+COMMENT ON COLUMN targetmode.ca IS '{"label": "Cash enabled", "description": "Valid for IEN", "summaryview": "true"}';
+COMMENT ON COLUMN targetmode.ct IS '{"label": "Counter Terrorism enabled", "description": "Valid for IEN", "summaryview": "true"}';
 COMMENT ON COLUMN targetmode.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN targetmode.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
