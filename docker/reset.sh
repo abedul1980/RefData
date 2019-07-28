e9862dfb06131f95b4228fdc98193810677b6edc
Mark Olliver <mark@digitalpatterns.io>
2019-07-28 19:36:47 +0100

    hm missing checks still

--- docker/reset.sh
+++ docker/reset.sh
@@ -28,7 +28,7 @@ export GOVERNANCE_READONLYUSER=${FLYWAY_PLACEHOLDERS_GOVERNANCE_READONLY_USER:-r
 export REFDB_URL="postgresql://${GOVERNANCE_OWNERNAME}:${GOVERNANCE_OWNERPASSWORD}@${POSTGRES_SERVER}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_OPTIONS}"
 
 # Clear reference DB
-psql ${REFDB_URL} -c "drop database ${REFDB_DBNAME};"
+psql ${REFDB_URL} -c "drop database if exists ${REFDB_DBNAME};"
 
 # Clear roles and schemas
-psql ${ROOT_URL} -c "drop table flyway_schema_history flyway_schema_history; drop role if exists ${GOVERNANCE_AUTHENTICATORUSER}; drop role if exists ${REFERENCE_AUTHENTICATORUSER}; drop role if exists ${GOVERNANCE_ANONUSER}; drop role if exists ${GOVERNANCE_SERVICEUSER}; drop role if exists ${GOVERNANCE_READONLYUSER}; drop role if exists ${GOVERNANCE_OWNERNAME}; drop role if exists ${REFERENCE_OWNERNAME}; drop role if exists ${REFERENCE_ANONUSER}; drop role if exists ${REFERENCE_SERVICEUSER}; drop role if exists ${REFERENCE_READONLYUSER};"
+psql ${ROOT_URL} -c "drop table if exists flyway_schema_history; drop role if exists ${GOVERNANCE_AUTHENTICATORUSER}; drop role if exists ${REFERENCE_AUTHENTICATORUSER}; drop role if exists ${GOVERNANCE_ANONUSER}; drop role if exists ${GOVERNANCE_SERVICEUSER}; drop role if exists ${GOVERNANCE_READONLYUSER}; drop role if exists ${GOVERNANCE_OWNERNAME}; drop role if exists ${REFERENCE_OWNERNAME}; drop role if exists ${REFERENCE_ANONUSER}; drop role if exists ${REFERENCE_SERVICEUSER}; drop role if exists ${REFERENCE_READONLYUSER};"
