877bb4b84d95d110102f5debf0b642e2db4314dc
Mark Olliver <mark@digitalpatterns.io>
2019-08-02 15:02:54 +0100

    updates ready for port

---
+++ schemas/reference/V81__portsize.sql
@@ -0,0 +1,20 @@
+CREATE TABLE portsize (
+  id INTEGER NOT NULL PRIMARY KEY,
+  rating INTEGER NOT NULL,
+  description VARCHAR(200) NOT NULL,
+  validfrom TIMESTAMP WITH TIME ZONE,
+  validto TIMESTAMP WITH TIME ZONE
+);
+
+-- Table comment
+COMMENT ON TABLE portsize IS '{"label": "Port Size", "description": "A scale categorizing the size of a port", "schemalastupdated": "01/08/2019", "dataversion": 1}';
+-- Column comments
+COMMENT ON COLUMN portsize.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
+COMMENT ON COLUMN portsize.rating IS '{"label": "Rating", "description": "Port Size Rating", "summaryview": "true"}';
+COMMENT ON COLUMN portsize.description IS '{"label": "Description", "description": "A definition of the rating.", "summaryview": "false"}';
+COMMENT ON COLUMN portsize.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN portsize.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
+
+-- GRANTs
+GRANT SELECT ON portsize TO ${serviceuser};
+GRANT SELECT ON portsize TO ${readonlyuser};
---
+++ schemas/reference/V82__port.sql
@@ -0,0 +1,61 @@
+CREATE TABLE port (
+  id INT4 NOT NULL PRIMARY KEY,
+  countryid INTEGER NULL REFERENCES country(id),
+  commandid INT4 NULL REFERENCES command(id),
+  locationid INT4 NULL REFERENCES location(id),
+  name VARCHAR(100) NOT NULL,
+  addressid UUID NULL,
+  subdivision VARCHAR(3),
+  municipality VARCHAR(60),
+  geolong NUMERIC,
+  geolat NUMERIC,
+  air BOOLEAN NOT NULL,
+  land BOOLEAN NOT NULL,
+  sea BOOLEAN NOT NULL,
+  rail BOOLEAN NOT NULL,
+  iata VARCHAR(3),
+  icao VARCHAR(10) NULL,
+  localcode VARCHAR (10) NULL,
+  unlocode VARCHAR(3) NOT NULL,
+  helipad BOOLEAN NULL,
+  fixedpcp BOOLEAN NULL,
+  portablepcp BOOLEAN NULL,
+  egates BOOLEAN NULL,
+  islargeport BOOLEAN NULL,
+  portsizeid int4 NULL,
+  validfrom TIMESTAMP WITH TIME ZONE NULL,
+  validto TIMESTAMP WITH TIME ZONE NULL
+);
+
+-- Table comment
+COMMENT ON TABLE port IS '{"label": "Worldwide Port list", "description": "The list of worldwide port/location list.", "schemalastupdated": "06/03/2019", "dataversion": 1}';
+-- Column comments
+COMMENT ON COLUMN port.id IS '{"label": "Identifier", "description": "Unique identifying column.", "summaryview": "false"}';
+COMMENT ON COLUMN port.countryid IS '{"label": "Linked country id", "description": "Country link to Country dataset.", "summaryview": "false"}';
+COMMENT ON COLUMN port.commandid IS '{"label": "Linked command id", "description": "Border force command link to command dataset.", "summaryview": "false"}';
+COMMENT ON COLUMN port.locationid IS '{"label": "Linked location id", "description": "Border force location link to location dataset.", "summaryview": "false"}';
+COMMENT ON COLUMN port.name IS '{"label": "Name", "description": "Site name", "summaryview": "true"}';
+COMMENT ON COLUMN port.addressid IS '{"label": "Linked address id", "description": "Links to non Home Office addresses.", "summaryview": "true"}';
+COMMENT ON COLUMN unlocode.subdivision IS '{"label": "County Unlocode", "description": "County from unlocode list", "summaryview": "false", "aliases": "region"}';
+COMMENT ON COLUMN port.municipality IS '{"label": "City IATA", "description": "Municipality/region from IATA", "summaryview": "true", "aliases": ["city", "region"]}';
+COMMENT ON COLUMN port.geolat IS '{"label": "Latitude", "description": "Geographic latitude", "summaryview": "false"}';
+COMMENT ON COLUMN port.geolong IS '{"label": "Longitude", "description": "Geographic longitude", "summaryview": "false"}';
+COMMENT ON COLUMN port.air IS '{"label": "Air", "description": "Is an Air Port with Runway or Helipad.", "summaryview": "true"}';
+COMMENT ON COLUMN port.land IS '{"label": "Land", "description": "Is a land border crossing.", "summaryview": "ture"}';
+COMMENT ON COLUMN port.sea IS '{"label": "Sea", "description": "Maritime port of entry.", "summaryview": "true"}';
+COMMENT ON COLUMN port.rail IS '{"label": "Rail", "description": "Rail  port of entry.", "summaryview": "true"}';
+COMMENT ON COLUMN port.iata IS '{"label": "IATA", "description": "IATA code", "summaryview": "true"}';
+COMMENT ON COLUMN port.icao IS '{"label": "ICAO code", "description": "ICAO code", "summaryview": "true"}';
+COMMENT ON COLUMN port.unlocode IS '{"label": "UN location code", "description": "UN location code", "summaryview": "true"}';
+COMMENT ON COLUMN port.helipad IS '{"label": "Helipad", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN port.fixedpcp IS '{"label": "Fixed PCP", "description": "Fixed PCP location.", "summaryview" : "false"}';
+COMMENT ON COLUMN port.portablepcp IS '{"label": "Portable PCP", "description": "Portable PCP location.", "summaryview" : "false"}';
+COMMENT ON COLUMN port.egates IS '{"label": "Egates", "description": "Egate location.", "summaryview" : "false"}';
+COMMENT ON COLUMN port.islargeport IS '{"label": "Large Port", "description": "Is a large port.", "summaryview" : "false"}';
+COMMENT ON COLUMN port.portsizeid IS '{"label": "BF port size", "description": "Links to Border Force port size list.", "summaryview" : "false"}';
+COMMENT ON COLUMN port.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
+COMMENT ON COLUMN port.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
+
+-- GRANTs
+GRANT SELECT ON port TO ${serviceuser};
+GRANT SELECT ON port TO ${readonlyuser};
