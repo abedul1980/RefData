e9713b45ee509b77f0ac3e2513843e722650837c
Ben Milnes <benjmilnes@outlook.com>
2019-08-28 14:20:23 +0100

    modified relevant powers to include Legal Gateway

--- schemas/reference/V46__relevantpowers.sql
+++ schemas/reference/V46__relevantpowers.sql
@@ -3,7 +3,8 @@ CREATE TABLE relevantpowers (
   code VARCHAR(40) NOT NULL,
   description TEXT NOT NULL,
   url VARCHAR(100),
-  sop BOOLEAN NOT NULL,
+  searchofperson BOOLEAN NOT NULL,
+  legalgateway BOOLEAN NOT NULL,
   validfrom TIMESTAMP WITH TIME ZONE,
   validto TIMESTAMP WITH TIME ZONE
 );
@@ -15,7 +16,8 @@ COMMENT ON COLUMN relevantpowers.id IS '{"label": "Identifier", "description": "
 COMMENT ON COLUMN relevantpowers.code IS '{"label": "Legal code", "description": "The legal code number.", "summaryview": "true"}';
 COMMENT ON COLUMN relevantpowers.description IS '{"label": "Description", "description": "A short description of the power.", "summaryview": "true"}';
 COMMENT ON COLUMN relevantpowers.url IS '{"label": "URL", "description": "Link to the full power in legislation.", "summaryview": "true"}';
-COMMENT ON COLUMN relevantpowers.sop IS '{"label": "sop", "description": "Valid power for search of person.", "summaryview": "false"}';
+COMMENT ON COLUMN relevantpowers.searchofperson IS '{"label": "sop", "description": "Valid power for search of person.", "summaryview": "false"}';
+COMMENT ON COLUMN relevantpowers.legalgateway IS '{"label": "legalgateway", "description": "Is this a legal gateway?", "summaryview": "false"}';
 COMMENT ON COLUMN relevantpowers.validfrom IS '{"label": "Valid from date", "description": "Item valid from date.", "summaryview" : "false"}';
 COMMENT ON COLUMN relevantpowers.validto IS '{"label": "Valid to date", "description": "Item valid to date.", "summaryview" : "false"}';
 
